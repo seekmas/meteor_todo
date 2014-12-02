6c202e8f82cab5b0daf3471ddf9d85f9f0cf9459
seekmas <seekmas@msn.cn>
2014-12-02 14:14:31 +0800

    finish

--- todos.js
+++ todos.js
@@ -2,6 +2,21 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
 
+    var DateFormats = {
+        simple: "YYYY/MM/DD",
+        normal: "dddd DD.MM.YYYY HH:mm"
+    };
+
+    UI.registerHelper("formatDate", function(datetime, format) {
+        if (moment) {
+            f = DateFormats[format];
+            return moment(datetime).format(f);
+        }
+        else {
+            return datetime;
+        }
+    });
+
     Template.body.helpers({
         tasks: function() {
             return Tasks.find({} , {sort: {createdAt: -1}});
