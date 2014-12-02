b7eed88a98862a1edd216f15014d3ea09f447615
seekmas <seekmas@msn.cn>
2014-12-02 13:48:10 +0800

    todo

---
+++ todos.js
@@ -0,0 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+
+    Template.body.helpers({
+        tasks: function() {
+            return Tasks.find({} , {sort: {createdAt: -1}});
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task": function(event) {
+            var text = event.target.text.value;
+            Tasks.insert({
+                text: text ,
+                createdAt: new Date()
+            });
+            console.log(event);
+            event.target.text.value= "";
+            return false;
+        }
+    });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
